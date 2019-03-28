7d20907930f1af4298db287a27e19aea04400154
tkielarbb <tomek.kielar00@gmail.com>
2019-03-28 19:05:04 +0100

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Tomasz Kielar on 28/03/2019.
+//  Copyright © 2019 Tomasz Kielar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
